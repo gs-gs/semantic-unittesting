25c9f9e3c203ba3426518c00519aa96494add329
Chris Gough <chris.gough@gosource.com.au>
2023-09-13 22:34:33 +1000

    clean up signals.py
    I left a noisy debug statement, it is no longer needed

--- backend/contenttester/signals.py
+++ backend/contenttester/signals.py
@@ -6,21 +6,16 @@ It might be smarter (less magic) to just overload Response.save()
 (rather than using signals like this).
 """
 from django.db.models.signals import post_save
-from django.dispatch import receiver
+# from django.dispatch import receiver
 import logging
 from .tasks import assess_response
 from .models import Response
 
 logger = logging.getLogger(__name__)
 
-
 def dispatch_evaluators(sender, **kwargs):
-    """wrapper that matches the signal signature"""
     response = kwargs["instance"]
-    response_id = response.pk
     for expectation in response.query.expectation_set.all():
-        logger.warn(f"DEBUG: {expectation}")  # DEBUG
-        expectation_id = expectation.pk
-        assess_response.delay(response_id, expectation_id)
+        assess_response.delay(response.pk, expectation.pk)
 
 post_save.connect(dispatch_evaluators, Response)
